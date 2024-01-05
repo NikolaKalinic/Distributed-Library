f5d9fcf28666cf246d9796e999e6997848bf039b
unknown <nikolaa.kalinic@gmail.com>
2024-01-05 14:41:08 +0100

    setup project

---
+++ CentralLibrary/src/main/java/com/cc/CentralLibrary/CentralLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.cc.CentralLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralLibraryApplication.class, args);
+	}
+
+}
---
+++ CityLibrary/src/main/java/com/cc/CityLibrary/CityLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.cc.CityLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityLibraryApplication.class, args);
+	}
+
+}
