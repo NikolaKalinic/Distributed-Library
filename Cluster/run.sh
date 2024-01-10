497004695b99f7f30296f90518650571724d174c
Nikola <nikolaa.kalinic@gmail.com>
2024-01-10 18:18:34 +0100

    added volume

--- Cluster/run.sh
+++ Cluster/run.sh
@@ -4,3 +4,5 @@ kubectl apply -f central_service.yaml
 kubectl apply -f city_deployment.yaml
 kubectl apply -f city_service.yaml
 kubectl apply -f ingress.yaml
+kubectl apply -f mysql_volume.yaml
+kubectl apply -f postgres_volume.yaml
