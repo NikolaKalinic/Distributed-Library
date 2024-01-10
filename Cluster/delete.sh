14b01f67d0fbc32519d08f652571193c6772dbc9
Nikola <nikolaa.kalinic@gmail.com>
2024-01-10 17:50:48 +0100

    added ingress

---
+++ Cluster/delete.sh
@@ -0,0 +1,4 @@
+kubectl delete service/central-app-service   
+kubectl delete service/city-app-service
+kubectl delete deployment.apps/central-app 
+kubectl delete deployment.apps/city-app 
--- Cluster/run.sh
+++ Cluster/run.sh
@@ -3,3 +3,4 @@ kubectl apply -f central_deployment.yaml
 kubectl apply -f central_service.yaml
 kubectl apply -f city_deployment.yaml
 kubectl apply -f city_service.yaml
+kubectl apply -f ingress.yaml
