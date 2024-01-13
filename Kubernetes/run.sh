697432cc3814a80f80f704eae4f2dae02d567df7
Nikola <nikolaa.kalinic@gmail.com>
2024-01-13 18:43:48 +0100

    update image on dockerhub to try to fix problem with ingress

--- Kubernetes/run.sh
+++ Kubernetes/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+kubectl apply -f mysql_volume.yaml
+kubectl apply -f postgres_volume.yaml
 kubectl apply -f central_deployment.yaml
 kubectl apply -f central_service.yaml
 kubectl apply -f city_deployment.yaml
 kubectl apply -f city_service.yaml
 kubectl apply -f ingress.yaml
-kubectl apply -f mysql_volume.yaml
-kubectl apply -f postgres_volume.yaml
